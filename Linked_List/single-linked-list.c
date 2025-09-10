6e0f5d6e1f5570c904b30affc8e71204d2f8923c
Chandrasekar Rajendran (LTIMINDTREE LIMITED) <v-crajendran@microsoft.com>
2025-09-10 23:57:49 +0530

    Added the multiple insert functions and delete node function

--- Linked_List/single-linked-list.c
+++ Linked_List/single-linked-list.c
@@ -25,9 +25,15 @@ void displayList(LINKED_LIST *head)
 {
     LINKED_LIST *p;
     
-    for(p=head; p!=NULL; p=p->link)
-        printf("%d -> ", p->info);
-    printf("NULL\n");
+    if(head != NULL) {
+        
+        for(p=head; p!=NULL; p=p->link)
+            printf("%d -> ", p->info);
+        printf("NULL\n");
+    }
+    else {
+        printf("List is Empty\n");
+    }
 }
 
 
@@ -42,13 +48,44 @@ void countNodes(LINKED_LIST *head)
     printf("Number of nodes in the list : %d\n",n);
 }
 
+LINKED_LIST *insertAtPos(LINKED_LIST *head,int pos, int data)
+{
+    LINKED_LIST *temp = (LINKED_LIST*)malloc(sizeof(LINKED_LIST));
+    LINKED_LIST *p;
+    int i;
+    
+    temp->info = data;
+    temp->link = NULL;
+    
+    for(p=head, i=1; i<pos-1 && p!=NULL; p=p->link)
+        i++;
+
+    if(pos == 1)
+    {
+        temp->link = head;
+        head = temp;
+        return head;
+    }
+    
+    temp->link = p->link;
+    p->link = temp;
+    
+    return head;
+}
+
 
 void searchNode(LINKED_LIST *head)
 {
     LINKED_LIST *p;
     int n=0,data;
     
-    printf("Enter the element to serached : ");
+    if(head == NULL)
+    {
+        printf("List is Empty\n");
+        return;
+    }
+    
+    printf("Enter the element to searched : ");
     scanf("%d",&data);
     
     for(p=head; p!=NULL; p=p->link) {
@@ -62,6 +99,69 @@ void searchNode(LINKED_LIST *head)
         printf("Number found at position %d in the list\n",n);
     else
         printf("Number not found in the list\n");
+}
+
+LINKED_LIST *insertBefore(LINKED_LIST *head,int val,int data)
+{
+    LINKED_LIST *temp = (LINKED_LIST*)malloc(sizeof(LINKED_LIST));
+    LINKED_LIST *p;
+    if(head == NULL)
+    {
+        printf("List is Empty\n");
+        return head;
+    }
+    
+    temp->info = data;
+
+    if(data == head->info)
+    {
+            temp->link = head;
+            head = temp;
+            return head;
+    }
+    
+    p = head;
+    
+    while(p->link != NULL) {
+        if(p->link->info == val)
+            break;
+        p = p->link;
+    }
+    
+    if(p->link == NULL) {
+        printf("%d is not found in list\n",val);    
+    }
+    else {
+        temp->link = p->link;
+        p->link = temp;
+    }
+    
+    return head;
+}
+
+void insertAfter(LINKED_LIST *head,int val,int data)
+{
+    LINKED_LIST *temp = (LINKED_LIST*)malloc(sizeof(LINKED_LIST));
+    LINKED_LIST *p;
+
+    temp->info = data;
+    temp->link = NULL;
+    
+    if(head != NULL) {
+        p=head;
+        while(p != NULL) {
+            if(p->info == val)
+                break;
+            p=p->link;
+        }
+        
+        temp->link = p->link;
+        p->link = temp;
+    }
+    else {
+        printf("List is Empty\n");
+    }
+    
 }
 
 LINKED_LIST *insertAtBegin(LINKED_LIST *head,int data)
@@ -119,10 +219,47 @@ LINKED_LIST *create_list(LINKED_LIST *head)
     return head;
 }
 
+LINKED_LIST *deleteNode(LINKED_LIST *head, int data)
+{
+    LINKED_LIST *temp,*p;
+    
+    if(head == NULL)
+    {
+        printf("List is Empty\n");
+        return head;
+    }
+    
+    if(head->info == data)
+    {
+        temp = head;
+        head = head->link;
+        free(temp);
+    }
+    
+    for(p=head; p->link != NULL; p=p->link)
+    {
+        if(p->link->info == data)
+            break;
+    }
+    
+    if(p->link == NULL)
+    {
+        printf("Element %d is not in the list\n",data);
+    }
+    else {
+        temp = p->link;
+        p->link = temp->link;
+        free(temp);
+    }
+    
+    return head;
+}
+
+
 int main() {
 	// your code goes here
     LINKED_LIST *head = NULL;
-    int choice,data,x,k;
+    int choice,data,val,pos;
     
     head = create_list(head);
     while(1) {
@@ -163,14 +300,27 @@ int main() {
                             scanf("%d",&data);
                             insertAtEnd(head, data);
                             break;
-            case ADD_AFTER: 
-            break;
-            case ADD_BEFORE: 
-            break;
-            case ADD_POS: 
-            break;
-            case DELETE: 
-            break;
+            case ADD_AFTER: printf("\nEnter the element to be inserted : ");
+                            scanf("%d",&data);
+                            printf("\nEnter after which element the value should be inserted : ");
+                            scanf("%d",&val);
+                            insertAfter(head,val,data);
+                            break;
+            case ADD_BEFORE: printf("\nEnter the element to be inserted : ");
+                             scanf("%d",&data);
+                             printf("\nEnter before which element the value should be inserted : ");
+                             scanf("%d",&val);
+                            head = insertBefore(head,val,data);
+                            break;
+            case ADD_POS:  printf("\nEnter the element to be inserted : ");
+                            scanf("%d",&data);
+                            printf("\nEnter the position the value should be inserted : ");
+                            scanf("%d",&pos);
+                            head = insertAtPos(head,pos,data);
+                            break;
+            case DELETE: printf("\nEnter the data to be deleted: ");          scanf("%d",&data);
+                         head = deleteNode(head,data);
+                         break;
             case REVERSE: 
             break;
             default: 
